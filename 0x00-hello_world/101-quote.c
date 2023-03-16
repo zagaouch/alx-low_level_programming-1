88aa4b99580e1dc18e2e23f99aa56541117528d0
youssef1lam <youlam1998@gmail.com>
2023-03-16 05:44:19 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,8 @@
+#include <unistd.h>
+int main(void)
+{
+char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+int len = sizeof(str) - 1;
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", len);
+return (1);
+}

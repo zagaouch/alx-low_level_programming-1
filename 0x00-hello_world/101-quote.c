2825b30841ca91caf8a12177242d5e61519ea45e
youssef1lam <youlam1998@gmail.com>
2023-03-16 05:53:52 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,5 @@
 #include <unistd.h>
-/* main - 101-quote.c
- *
- * Return: Always returns 1
- */
+/* This program prints a message to the standard error. */
 int main(void)
 {
 char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";

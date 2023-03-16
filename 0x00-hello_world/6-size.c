bacbcc570399b2fc30ac33ba5889f53906904892
youssef1lam <youlam1998@gmail.com>
2023-03-16 05:39:58 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Prints the size of various types on the computer
+ * main - Prints the size of various types on the computerr
  *
  * Return: Always 0 (Success)
  */
